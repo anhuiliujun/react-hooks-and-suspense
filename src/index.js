bf3c146ba3e625efd659f7ba6c792eb0a9e559df
issaclau <issaclau3@gmail.com>
2018-11-10 16:17:34 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./demos/13-suspense-fundamentals";
+
+import "./styles.css";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
